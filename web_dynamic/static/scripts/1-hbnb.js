7da94ebebc328e4b851049be67179d3c30c81e9d
bchen528 <bchen720@gmail.com>
2018-09-08 04:13:48 +0000

    semistandard compliant

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,43 +1,36 @@
-document.addEventListener('DOMContentLoaded', function (){
+document.addEventListener('DOMContentLoaded', function () {
   let displayObjects = {};
-  let checkCounter = 0
-  let $h4 = $('div.amenities h4')
+  let $h4 = $('div.amenities h4');
 
-  $('input').each(function(idx, ele){
-    let id = $(this).attr('data-id')
-    let name = $(this).attr('data-name')
+  $('input').each(function (idx, ele) {
+    let id = $(this).attr('data-id');
+    let name = $(this).attr('data-name');
 
     // load dictionary
     displayObjects[id] = {name: name, checked: false};
 
     // set change method on checkboxes
-    $(ele).change(function() {
-
-      let delimiter = '<span class="delim">, </span>'
-      $('h4 span.delim').remove()
+    $(ele).change(function () {
+      let delimiter = '<span class="delim">, </span>';
+      $('h4 span.delim').remove();
 
       if (this.checked) {
-        console.log("checked")
+        console.log('checked');
         displayObjects[id]['checked'] = true;
-        $h4.append("<span id="+id+">" + name + "</span>"); 
+        $h4.append('<span id=' + id + '>' + name + '</span>');
       } else {
-        console.log('unchecked')
+        console.log('unchecked');
         displayObjects[id]['checked'] = false;
-        $('span#'+id).remove()
+        $('span#' + id).remove();
       }
 
-      let length = $('h4 > span').length
-      console.log($('h4 > span').length)
-      $('div.amenities h4 span').each(function(idx, ele){
-          if (idx < length - 1 ) {
-            $(this).append('<span class=delim>, </span>');
-          }
-          
+      let length = $('h4 > span').length;
+      console.log($('h4 > span').length);
+      $('div.amenities h4 span').each(function (idx, ele) {
+        if (idx < length - 1) {
+          $(this).append(delimiter);
+        }
       });
-
     });
-
   });
-
 });
-
